db4573fc73d3e0ce06becc851a802a24e486a36a
Jason Mulvenna <jason.mulvenna@gmail.com>
2016-03-04 14:15:38 +1000

    Bug fix

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [me.raynes/fs "1.4.6"]
-                 [clj-uniprot "0.1.1"]
+                 [clj-uniprot "0.1.2"]
                  [org.clojure/tools.cli "0.3.3"]]
   :main ^:skip-aot uniprot2fasta.core
   :target-path "target/%s"
